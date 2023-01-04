1598fc66df533d450e9b4818ddac0e77f2410139
Joey Tsui <joeytsui131@gmail.com>
2023-01-04 16:40:22 -0500

    phase 3

--- skeleton 8/phase_2_enumerable.js
+++ skeleton 8/phase_2_enumerable.js
@@ -3,3 +3,36 @@ Array.prototype.myEach = function(cb){
         cb(this[i])
     }
 }
+
+let bob = [1,2,3,4,5].myEach(function(num){
+    console.log(num * num)
+})  
+
+Array.prototype.myMap = function(cb) {
+    let retArr = []
+
+    this.myEach(function(ele){
+        retArr.push(cb(ele))
+    })
+
+    return retArr
+}
+
+console.log([1, 2, 3, 4, 5].myMap(function (num) {
+    return num * 2
+}))
+
+Array.prototype.myReduce = function(cb, iVal) {
+    if (iVal === undefined) {
+        iVal = this.shift()
+    }
+
+    this.myEach(function(ele) {
+        iVal = cb(iVal, ele)
+    })
+    return iVal
+}
+
+console.log([1, 2, 3].myReduce(function (acc, el) {
+    return acc + el;
+})); 
